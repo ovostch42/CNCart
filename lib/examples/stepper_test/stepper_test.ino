3751d81d5fb68d9f2dfee49062e1e4e50ed80e14
ovostch42 <mattveyxes@mail.ru>
2026-02-27 16:26:59 +0300

    Add files via upload

---
+++ lib/examples/stepper_test/stepper_test.ino
@@ -0,0 +1,19 @@
+#include <MOSIKStep.h>
+
+MOSIKStep cpushp(6,7,4,3,9,18.5,350,3500,6.24);
+
+void setup(){
+  cpushp.turn(90,1);
+  cpushp.turn(90,0);
+}
+
+void loop(){
+  /*
+  digitalWrite(6,HIGH);
+  digitalWrite(7,HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(6,LOW);
+  digitalWrite(7,LOW);
+  delayMicroseconds(1000);
+  */
+}
